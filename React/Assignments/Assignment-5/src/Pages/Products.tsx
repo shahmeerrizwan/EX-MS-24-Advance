11fe35f0e14ab75a622b59e27dd566a6e93f730a
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-06-29 02:13:22 +0500

    add-image

--- React/Assignments/Assignment-5/src/Pages/Products.tsx
+++ React/Assignments/Assignment-5/src/Pages/Products.tsx
@@ -36,7 +36,7 @@ export default function Products() {
 
 <div className="containerr">
   <div className="card">
-    <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXMlMjBuaWtlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" alt="" />
+    <img src="https://images.unsplash.com/photo-1559181567-c3190ca9959b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fG9iamVjdHxlbnwwfHwwfHx8MA%3D%3D" alt="" />
     <div className="card-body">
       <div className="row">
         <div className="card-title">
