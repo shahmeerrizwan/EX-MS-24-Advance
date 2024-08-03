60ce27032cbe758db73bf88e7ad7eb9a92d7ebf6
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-03 23:25:25 +0500

    Redux-ToolKit

---
+++ React/13-Redux/src/Components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function index() {
+    return (
+        <>
+            <div className='nav'>
+                <h1>Redux Toolkit</h1>
+            </div>
+        </>
+    )
+}
