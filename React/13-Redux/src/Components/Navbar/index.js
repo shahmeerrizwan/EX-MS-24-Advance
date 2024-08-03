664a4b779b736a6227b8e4e4467a6410701ba397
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-03 23:34:46 +0500

    Redux-ToolKit

--- React/13-Redux/src/Components/Navbar/index.js
+++ React/13-Redux/src/Components/Navbar/index.js
@@ -7,6 +7,12 @@ export default function index() {
             <div className='nav'>
                 <h1>Redux Toolkit</h1>
             </div>
+            <div className='button'>
+                <button className='btn'>Red Mode</button>
+                <button className='btn'>Black Mode</button>
+                <button className='btn'>Yellow Mode</button>
+                <button className='btn'>Light Mode</button>
+            </div>
         </>
     )
 }
