a6158fe28837f710b742eba4e6bb9bcad0e8f6e7
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-06 01:10:01 +0500

    Redux

--- React/13-Redux/src/Store/index.js
+++ React/13-Redux/src/Store/index.js
@@ -2,16 +2,31 @@ import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./ThemeSlice";
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import {
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
 
 
 const persistConfig = {
     key: 'root',
+    version: 1,
     storage,
 }
 const persistedReducer = persistReducer(persistConfig, themeReducer)
 
 const store = configureStore({
-    reducer: persistedReducer
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
 
 
