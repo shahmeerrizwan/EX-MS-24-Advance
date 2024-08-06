35cb4cf5b48bbef3bf28adddffdb551f16082ce6
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-07 02:52:17 +0500

    Amazon-Clone

---
+++ React/Assignments/Assignment-7/src/Components/Cards/Slider/Slider.tsx
@@ -0,0 +1,296 @@
+import React from 'react'
+import './Slider.css'
+import img1 from '../../../Assets/boxb101.jpg'
+
+export default function Slider() {
+  return (
+    <>
+        <div className="box2 blockbuster_deals">
+                    <div>
+                        <div>
+                            <h2>Blockbuster deals</h2>
+                            <span>See all deals</span>
+                        </div>
+                        <div className="mini_slide-block">
+                            <div className="left_img_button" id="slideButton-b">
+                                <i className="fa-solid fa-angle-left"></i>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src={img1} alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Up to 24% off</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>Samsung Galaxy M34</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src="./assest/boxb102.jpg" alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Up to 45% off</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>Redmi 12C | Starting from 7699 incl...</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src="./assest/boxb103.jpg" alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Up to 76% off</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>Top headsets from Oneplus, Samsung...</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src="./assest/boxb104.jpg" alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Up to 38% off</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>Daily Essentials</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src="./assest/boxb105.jpg" alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Up to 43% off</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>Skin - Biotique, Cetaphil, Himalaya...</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src="./assest/boxb106.jpg" alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Up to 55% off</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>JBL Audio Freedom Sale Deals...</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src="./assest/boxb107.jpg" alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Deal of the Day</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>OnePlus Nord CE 3 5G | Latest launch...</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src="./assest/boxb108.jpg" alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Up to 58% off</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>Alexa Devices - Echo and Fire TV..</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src="./assest/boxb109.jpg" alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Up to 63% off</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>Best Styles in Footwear and Handbags...</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src="./assest/boxb110.jpg" alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Up to 60% off</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>Top Offers on Mice & Keyboards</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src="./assest/boxb111.jpg" alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Up to 76% off</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>Best Selling Massagers</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src="./assest/boxb112.jpg" alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Up to 61% off</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>Lowest Ever Price on PS5 Console</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src="./assest/boxb113.jpg" alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Up to 68% off</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>Cycle and Cycling accessories: Lifelong...</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src="./assest/boxb114.jpg" alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Up to 63% off</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>Gas Stoves and Hobs from Top Brands</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <a href="/">
+                                        <div>
+                                            <img src="./assest/boxb115.jpg" alt="" />
+                                        </div>
+                                    </a>
+                                </div>
+                                <div>
+                                    <div>
+                                        <span><button>Up to 38% off</button></span>
+                                        <span>Great Freedom Sale</span>
+                                    </div>
+                                    <div>
+                                        <span>Lowest Prices of the year on Smartwa...</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="right_img_button" id="slideButton-b">
+                                <i className="fa-solid fa-angle-right"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </>
+  )
+}
--- React/Assignments/Assignment-7/src/Screens/MainScreen.tsx
+++ React/Assignments/Assignment-7/src/Screens/MainScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
 import Home from '../Components/Home/Home'
 import Card1 from '../Components/Cards/Card 1/Card1'
+import Slider from '../Components/Cards/Slider/Slider'
 
 export default function MainScreen() {
   return (
@@ -9,6 +10,7 @@ export default function MainScreen() {
       <Navbar/>
        <Home/>
        <Card1/>
+       <Slider/>
     </>
   )
 }
