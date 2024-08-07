52beedf710606be04f3fefd9aa1ee4fd0ab05b15
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-08 03:44:57 +0500

    Amazon-Clone

--- React/Assignments/Assignment-7/src/Store/CartSlice.tsx
+++ React/Assignments/Assignment-7/src/Store/CartSlice.tsx
@@ -17,7 +17,6 @@ export const cartSlice = createSlice({
   },
 })
 
-// Action creators are generated for each case reducer function
 export const { addToCart } = cartSlice.actions
 
 export default cartSlice.reducer
