89e97d0377bb54ab0b7c27ca0d5c2c631e917710
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-10 02:24:15 +0500

    Amazon-Clone

--- React/Assignments/Assignment-7/src/Components/Navbar/Navbar.tsx
+++ React/Assignments/Assignment-7/src/Components/Navbar/Navbar.tsx
@@ -244,7 +244,7 @@ export default function Navbar() {
         <div className="summary-total-items"><span className="total-items"></span> Items in your Cart</div>
         <div className="summary-subtotal">
           <div className="subtotal-title">Subtotal</div>
-          <div className="subtotal-value final-value" id="basket-subtotal">130.00</div>
+          <div className="subtotal-value final-value">${total.toFixed(2)}</div>
           <div className="summary-promo hide">
             <div className="promo-title">Promotion</div>
             <div className="promo-value final-value" id="basket-promo"></div>
@@ -261,10 +261,10 @@ export default function Navbar() {
         </div>
         <div className="summary-total">
           <div className="total-title">Total</div>
-          <div className="total-value final-value" id="basket-total">130.00</div>
+          <div className="total-value final-value" id="basket-total">${total.toFixed(2)}</div>
         </div>
         <div className="summary-checkout">
-          <button className="checkout-cta">Go to Secure Checkout</button>
+          <button className="checkout-cta">Proceed to checkout</button>
         </div>
       </div>
     </aside>
