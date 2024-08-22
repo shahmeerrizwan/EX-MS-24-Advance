06ef56944f83b6238c29f7d384490d0061f98e8e
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-23 03:43:34 +0500

    Amazon-Clone

--- React/Assignments/Assignment-7/src/Components/Home/Home.tsx
+++ React/Assignments/Assignment-7/src/Components/Home/Home.tsx
@@ -11,45 +11,44 @@ import img8 from '../../Assets/bigImg4.jpg'
 
 export default function Home() {
   return (
-    <>
+  <>
     <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img className="d-block w-100" src={img1} alt="First slide" />
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img className="d-block w-100" src={img1} alt="First slide" />
+        </div>
+       <div className="carousel-item">
+         <img className="d-block w-100" src={img2} alt="Second slide" />
+       </div>
+       <div className="carousel-item">
+         <img className="d-block w-100" src={img3} alt="Third slide" />
+       </div>
+        <div className="carousel-item">
+        <img className="d-block w-100" src={img4} alt="Third slide" />
+        </div>
+        <div className="carousel-item">
+        <img className="d-block w-100" src={img5} alt="Third slide" />
+        </div>
+        <div className="carousel-item">
+        <img className="d-block w-100" src={img1} alt="Third slide" />
+        </div>
+       <div className="carousel-item">
+        <img className="d-block w-100" src={img7} alt="Third slide" />
+       </div>
+       <div className="carousel-item">
+        <img className="d-block w-100" src={img8} alt="Third slide" />
+       </div>
+      </div>
+       <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+       <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+       <span className="sr-only">Previous</span>
+       </a>
+       <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+       <span className="carousel-control-next-icon" aria-hidden="true"></span>
+       <span className="sr-only">Next</span>
+       </a>
     </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src={img2} alt="Second slide" />
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src={img3} alt="Third slide" />
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src={img4} alt="Third slide" />
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src={img5} alt="Third slide" />
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src={img1} alt="Third slide" />
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src={img7} alt="Third slide" />
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src={img8} alt="Third slide" />
-    </div>
-  </div>
-  <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="sr-only">Previous</span>
-  </a>
-  <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="sr-only">Next</span>
-  </a>
-</div>
    <div className="blur_img" ></div>
-   
-    </>
+  </>
   )
 }
